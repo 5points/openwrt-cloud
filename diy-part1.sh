e4c8c523dbbf5eb7cc3ddc12a5a531d490742fa3
5points <1803962047@qq.com>
2020-11-19 01:45:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,6 +28,6 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/l
 # Add a Smartinfo to plugin
 pushd package/
 git clone https://github.com/KFERMercer/OpenWrt.git ~/package/kferm/luci-app-smartinfo
-cp -r ~/work/openwrt-cloud/openwrt-cloud/openwrt/packagekferm/luci-app-smartinfo "~/work/openwrt-cloud/openwrt-cloud/openwrt/package/lean"
+cp -r ~/work/openwrt-cloud/openwrt-cloud/openwrt/package/kferm/luci-app-smartinfo "~/work/openwrt-cloud/openwrt-cloud/openwrt/package/lean"
 popd
 # git clone https://github.com/ximliu/luci-app-smartinfo.git package/lean/luci-app-smartinfo
