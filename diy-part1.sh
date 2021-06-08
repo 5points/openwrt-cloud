35f69ce8b3d8f3a0343a642c7110455ab4b3d125
5points <1803962047@qq.com>
2021-06-08 19:48:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,13 +28,14 @@ rm -rf ../lean/luci-app-docker
 git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
 git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
-
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add a Smartinfo to plugin
 svn co https://github.com/KFERMercer/OpenWrt/trunk/package/kferm/luci-app-smartinfo
 
+# Add subconverter
+git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # Add a plugin and theme
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
@@ -44,20 +45,5 @@ git clone https://github.com/project-lede/luci-app-godproxy.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone https://github.com/rufengsuixing/luci-app-onliner.git
 git clone https://github.com/esirplayground/luci-app-poweroff.git
-git clone https://github.com/xiaorouji/openwrt-passwall.git
 popd
 
-# Add a plugin and theme
-# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
-# git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon.git package/lean/luci-theme-atmaterial-ColorIcon
-# git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
-# git clone https://github.com/project-lede/luci-app-godproxy.git package/lean/luci-app-godproxy
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
-# git clone https://github.com/rufengsuixing/luci-app-onliner.git package/lean/luci-app-onliner
-# git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
-# git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
-# git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
-
-
-# add luci-app-smartinfo
-# git clone https://github.com/iamaluckyguy/luci-app-smartinfo.git package/lean/luci-app-smartinfo
