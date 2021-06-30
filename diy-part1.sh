80efabfeea6ee0d3ab4cfadfec45a85e6ae922fe
5points <1803962047@qq.com>
2021-06-30 14:10:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -100,3 +100,4 @@ sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
+./scripts/feeds update -a
