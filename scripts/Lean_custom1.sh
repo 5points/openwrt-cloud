73ddd12dc3cccb490f0006a33763a8e2cb34942f
5points <8995110+5points@users.noreply.github.com>
2022-08-04 14:27:33 +0800

    Update Lean_custom1.sh

--- scripts/Lean_custom1.sh
+++ scripts/Lean_custom1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2022 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: Lean_diy2.sh
+# File name: Lean_custom1.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
