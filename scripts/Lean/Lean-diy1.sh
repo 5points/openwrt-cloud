4e3df48ceb3649ea600ddd14246c29e20d3ebac8
5points <8995110+5points@users.noreply.github.com>
2022-09-03 23:59:21 +0800

    Update Lean-diy1.sh

--- scripts/Lean/Lean-diy1.sh
+++ scripts/Lean/Lean-diy1.sh
@@ -24,8 +24,8 @@ pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
-rm -rf ../lean/luci-app-kodexplorer
+git clone --depth=1 https://github.com/Lienol/openwrt-package lienol-package
+rm -rf ../lean/lienol-package/luci-app-kodexplorer
 
 # Add luci-app-dockerman
 rm -rf ../lean/luci-app-docker
