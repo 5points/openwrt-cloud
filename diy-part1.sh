edacf04dcef991af8607e570660b93e9e01b2193
5points <8995110+5points@users.noreply.github.com>
2023-03-09 19:28:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -64,7 +64,8 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git
 
 # theme source from https://github.com/kenzok8/openwrt-packages
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-mcat
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-mcat
+svn co https://github.com/gd0772/package/trunk/theme/luci-theme-opentomcat
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-tomato
 
 popd
