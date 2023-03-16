06055bf79f74d72b0e5777307df2392f501315d9
5points <8995110+5points@users.noreply.github.com>
2023-03-16 23:57:47 +0800

    Update Lean_custom-new-kernel-diy2.sh

--- scripts/Lean/Lean_custom-new-kernel-diy2.sh
+++ scripts/Lean/Lean_custom-new-kernel-diy2.sh
@@ -66,7 +66,7 @@ sed -i 's/挂载 SMB 网络共享/挂载共享/g' feeds/luci/applications/luci-a
 sed -i 's/msgstr "UPnP"/msgstr "UPnP设置"/g' feeds/luci/applications/luci-app-upnp/po/zh-cn/upnp.po
 
 # Modify the word to 'luci-app-wizard'
-#sed -i 's/entry({"admin", "wizard"}, cbi("wizard/entry({"admin", "system", "wizard"}, cbi("system/g' package/lean/luci-app-wizard/luasrc/controller/wizard.lua
+sed -i 's/entry({"admin", "wizard"}/entry({"admin", "system", "wizard"}/g' package/lean/luci-app-wizard/luasrc/controller/wizard.lua
 
 # Modfiy luci-app-smartdns 
 # download 'cunstom-config' to smartdns
